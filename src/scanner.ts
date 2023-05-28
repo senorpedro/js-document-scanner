21473674d43a0901bbf61c553dcb3642c9a6f52b
Peter Schweizer <peter.schweizer@gmail.com>
2023-05-28 20:49:24 +0200

    fixed display

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,25 +1,41 @@
 import cv from "@techstark/opencv-js";
 
+/**
+ * TODO
+ *  - expose some nice easy-to-use API
+ *  -
+ */
+
+// TODO find proper way to inject necessary HTHML
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <canvas id="canvasElement"></canvas>
 `;
 
-let width = 320,
-  height = 320;
-
 const videoElement = document.createElement("video");
+videoElement.autoplay = true;
 
 const canvasElement = document.getElementById(
   "canvasElement"
 ) as HTMLCanvasElement;
 
+var constraints = {
+  video: {
+    width: { ideal: 4096 },
+    height: { ideal: 2160 },
+  },
+};
+
 // Get access to the camera stream
 navigator.mediaDevices
-  .getUserMedia({ video: true })
+  .getUserMedia(constraints)
   .then(function (stream) {
     // once we have the camera, we start detecting documents
+    const settings = stream.getVideoTracks()[0].getSettings();
+    console.log(
+      `settings width = ${settings.width}px, height = ${settings.height}px`
+    );
     videoElement.srcObject = stream;
-    videoElement.play();
+    // videoElement.play();
   })
   .catch(function (error) {
     console.error("Error accessing the camera: ", error);
@@ -33,25 +49,20 @@ videoElement.addEventListener("canplay", (ev) => {
   // video stream just started
   isStreaming = true;
 
-  height = videoElement.videoHeight / (videoElement.videoWidth / width);
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+  canvasElement.style.width = videoElement.videoWidth + "px";
+  canvasElement.style.height = videoElement.videoHeight + "px";
 
-  runDrawingHandler();
+  drawImage();
 });
-
-function runDrawingHandler() {
-  requestAnimationFrame(() => {
-    drawImage();
-    runDrawingHandler();
-  });
-}
 
 function drawImage() {
   const context = canvasElement.getContext("2d") as CanvasRenderingContext2D;
-  console.log("drawing");
-  if (width && height) {
-    console.log("drawing");
-    canvasElement.width = width;
-    canvasElement.height = height;
-    context.drawImage(videoElement, 0, 0, width, height);
-  }
+
+  // scale
+  context.drawImage(videoElement, 0, 0);
+  requestAnimationFrame(() => {
+    drawImage();
+  });
 }
